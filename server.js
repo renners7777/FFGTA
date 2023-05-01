efee16d85c4666ba5e627a555bdb5d68a38b8b29
Chris <104800006+renners7777@users.noreply.github.com>
2023-05-01 19:46:39 +0100

    Added .env file

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
